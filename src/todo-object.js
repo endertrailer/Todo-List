2ef5d3bccd00acf7465286dc60e7fa0fa955c725
endertrailer <anismasoom80@gmail.com>
2024-02-29 15:10:07 +0530

    fix a infinite loop

--- src/todo-object.js
+++ src/todo-object.js
@@ -1,35 +1,24 @@
-let tasks = [];
-
-// Load tasks from localStorage on script load
-window.addEventListener('DOMContentLoaded', () => {
-    const storedTasks = JSON.parse(localStorage.getItem('tasks'));
-    if (storedTasks) {
-        tasks = storedTasks;
+let tasks = [{title: 'walking'}, {title: 'typing'}, {title: 'studying'}];
+let storedTasks = JSON.parse(localStorage.getItem('task'));
+if(storedTasks){
+    tasks = storedTasks;
+}
+export function createTodo(title){
+    
+    if(title !== undefined){
+        
+        tasks.push({title: title});
     }
-});
-
-export function getTasks() {
+    localStorage.setItem('task', JSON.stringify(tasks));
     return tasks;
 }
 
-export function saveTasks() {
-    localStorage.setItem('tasks', JSON.stringify(tasks));
-}
-
-export function createTodo(title) {
-    if (title !== undefined) {
-        tasks.push({ title: title });
-        saveTasks(); // Save tasks to localStorage after modification
-    }
-    return tasks;
-}
-
-export function addDate(date, index) {
+export function addDate(date, index){
     tasks[index].date = date;
-    saveTasks(); // Save tasks to localStorage after modification
+    localStorage.setItem('task', JSON.stringify(tasks));
 }
 
-export function removeObject(index) {
-    tasks.splice(index, 1);
-    saveTasks(); // Save tasks to localStorage after modification
+export function removeObject(index){
+    tasks.splice(index,1);
+    localStorage.setItem('task', JSON.stringify(tasks));
 }
